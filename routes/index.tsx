ff2aa7d3c4d0c7551e7d7f00cd97d4c03ca472d4
Andrey Radkevich <andrey@48pacific.com>
2022-08-03 01:50:37 +0300

    add-counter

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 /** @jsx h */
 import { h } from "preact";
+import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
         file, and refresh.
       </p>
+      <Counter start={3} />
     </div>
   );
 }
